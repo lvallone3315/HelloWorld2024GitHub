7239bd41245314d3c47b17c6bd936fd0d38c8c06
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2024-09-06 18:18:35 -0400

    added a print

--- HelloWorld2024local/HelloWorld2024local.cpp
+++ HelloWorld2024local/HelloWorld2024local.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "  adding a print to repo created in github\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
